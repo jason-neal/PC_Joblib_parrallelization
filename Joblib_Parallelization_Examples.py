338887a5ac48c91659ea09c0fcf8fd85bb9b7702
Jason Neal <jason.neal@astro.up.pt>
2016-05-20 16:42:06 +0100

    Begin Notebook to run examples.

---
+++ Joblib_Parallelization_Examples.py
@@ -0,0 +1,39 @@
+
+# coding: utf-8
+
+# # Embarisingly Parallel for loops using Joblib
+# 
+
+# In[2]:
+
+from joblib import Parallel, delayed
+import timeit
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
+
+# # Convolution Example
+# 
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
